a2d08d7ab9452e8caf190d14667639d89a203891
DivyanshD-30 <iamdivyanshdwivedi@gmail.com>
2025-04-12 20:51:49 +0530

    project and home ui integration and socket frontend

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '../screens/Login'
 import Register from '../screens/Register'
+import Project from '../screens/Project'
+import UserAuth from '../auth/UserAuth'
+import Home from '../screens/Home'
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
 
             <Routes>
+            <Route path="/" element={<UserAuth><Home /></UserAuth>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/project" element={<UserAuth><Project /></UserAuth>} />
             </Routes>
 
         </BrowserRouter>
